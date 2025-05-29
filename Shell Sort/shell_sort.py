196f26779341d0d3400293390f3834eb25d67bb7
Jardson Alan <jardsonalan132@gmail.com>
2025-05-28 21:32:23 -0300

    refactor: melhoria na implementação do shell sort

--- Shell Sort/shell_sort.py
+++ Shell Sort/shell_sort.py
@@ -1,34 +1,22 @@
-import math
-
 # Shell Sort
-def shell_sort(list: list[int]) -> list[int]:
-    # Divide o tamanho da lista pela metade para pegar o intervalo da operação
-    # Utiliza o método ceil, da biblioteca math, para pegar o teto da divisão
-    gap = math.ceil(len(list) / 2)
+def shell_sort(lista: list[int]) -> list[int]:
+  gap = len(lista) // 2 # Divide a lista pela metade para definir o primeiro intervalo (gap)
 
-    # Verifica se o gap é maior que 0 e executa o laço
-    while gap > 0:
-        # Utiliza um for in range para percorrer de 0 até o tamanho da lista, menos o gap
-        for index in range(0, len(list) - gap):
-            # Variável auxiliar que armazena o valor atual da lista
-            aux = list[index]
+  while gap > 0: # Executa o loop enquanto o valor do gap for maior que 0
+    for i in range(len(lista)): # Percorre os index da lista do início ao fim
+      aux = lista[i] # Variável auxiliar que armazena o valor da vez
 
-            # Verifica se o próximo elemento, que está a uma certa distância, é menor que o atual
-            if list[index + gap] < aux:
-                # Operação que executa a troca dos elementos
-                # A posição atual recebe o valor da posição posterior (com a distância gap), se ele for menor
-                list[index] = list[index + gap]
-                # O valor da posição posterior (com a distância gap), recebe o valor atual que está armazenado na váriavel auxiliar (aux)
-                list[index + gap] = aux
+      # Verifica se o index atual é maior ou igual ao gap
+      # e se o valor localizado 'gap' posições antes é maior que o valor atual (aux)
+      while i >= gap and lista[i - gap] > aux:
+        lista[i] = lista[i - gap] # Move o valor anterior (mais distante) para frente
+        i -= gap # Volta 'gap' posições para continuar a verificação
 
-        # A cada operação subtrai 1 do gap
-        gap -= 1
+      lista[i] = aux # Insere o valor original (aux) na posição correta após os deslocamentos
+    
+    gap //= 2 # Divide o valor do gap pela metade a cada iteração
+   
+  return lista # Retorna a lista ordenada
 
-    return list
-
-lista_desordenada = [3, 1, 5, 10, 15, 90, 2, 23, 8]
-print("Lista Desordenada")
-print(lista_desordenada)
-lista_ordenada = shell_sort(lista_desordenada)
-print("Lista Ordenada")
-print(lista_ordenada)
+lista_desordenada = [5, 16, 20, 12, 3, 8, 9, 17, 19, 7]
+print(shell_sort(lista_desordenada))
